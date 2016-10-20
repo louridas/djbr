700186f892f4c69d41fcb535ae5d78018ddc75e9
Panos Louridas <louridas@gmail.com>
2016-10-20 10:31:24 +0300

    changed static root

--- project_site/settings.py
+++ project_site/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_ROOT), 'static/')
 
 try:
     from .site_config import *
